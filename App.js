ed1f28e12fcb0345f9405e4db72a0fc2b76a9ed1
itay-az <65295205+itay-az@users.noreply.github.com>
2022-12-21 13:00:48 +0200

    adding ui and hooks

--- App.js
+++ App.js
@@ -1,14 +1,87 @@
 import React, {useState} from 'react';
 import {View,TouchableOpacity,Text} from 'react-native';
 import styles from './style';
+import { firstRow,secondRow,thirdRow,fourthRow } from './Arr';
+import * as math from 'mathjs';
 
 const App = () => {
+  const [title,setTitle] = useState('');
+  const [ans,setAns] = useState(null);
+
+  const handleClick = (txt) => {
+    console.log()
+    if(txt === '=') {
+      setAns(math.evaluate(title));
+      setTitle(ans);
+    } 
+    else if(txt ==='CLR') {
+      setTitle('');
+    }
+    else if(txt ==='+') {
+      setTitle(title + txt);
+    }
+    else if(txt ==='-') {
+      setTitle(title + txt);
+    }
+    else if(txt ==='*') {
+      setTitle(title + txt);
+    }
+    else if(txt ==='/') {
+      setTitle(title + txt);
+    }
+    else{
+      setTitle(title + txt)
+    }
+  }
 
 
 
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <View style={styles.resultRow}>
+        <Text style={styles.text} >{title}</Text>
+      </View>
+      <View style={styles.resultRow}>
+        <Text style={styles.text} >{ans}</Text>  
+      </View>
+
+      <View style={styles.row}>
+        {firstRow.map((btn => {
+          return(
+            <TouchableOpacity onPress={() => handleClick(btn.txt)} style={styles.btn}>
+            <Text style={styles.text}>{btn.txt}</Text>
+          </TouchableOpacity>
+          )
+        }))}
+      </View>
+      <View style={styles.row}>
+        {secondRow.map((btn => {
+          return(
+            <TouchableOpacity onPress={() => handleClick(btn.txt)} style={styles.btn}>
+            <Text style={styles.text}>{btn.txt}</Text>
+          </TouchableOpacity>
+          )
+        }))}
+      </View>
+      <View style={styles.row}>
+        {thirdRow.map((btn => {
+          return(
+            <TouchableOpacity onPress={() => handleClick(btn.txt)} style={styles.btn}>
+            <Text style={styles.text}>{btn.txt}</Text>
+          </TouchableOpacity>
+          )
+        }))}
+      </View>
+      <View style={styles.row}>
+        {fourthRow.map((btn => {
+          return(
+            <TouchableOpacity onPress={() => handleClick(btn.txt)} style={styles.btn}>
+            <Text style={styles.text}>{btn.txt}</Text>
+          </TouchableOpacity>
+          )
+        }))}
+      </View>
+
     </View>
   )
 }
---
+++ Arr.js
@@ -0,0 +1,28 @@
+export const firstRow = [
+    {id:1,txt:'7'},
+    {id:2,txt:'8'},
+    {id:3,txt:'9'},
+    {id:4,txt:'='},
+];
+
+export const secondRow = [
+    {id:5,txt:'4'},
+    {id:6,txt:'5'},
+    {id:7,txt:'6'},
+    {id:8,txt:'+'},
+];
+
+export const thirdRow = [
+    {id:9,txt:'1'},
+    {id:10,txt:'2'},
+    {id:11,txt:'3'},
+    {id:12,txt:'-'},
+];
+
+export const fourthRow = [
+    {id:9,txt:'CLR'},
+    {id:10,txt:'0'},
+    {id:11,txt:'*'},
+    {id:12,txt:'/'},
+];
+
